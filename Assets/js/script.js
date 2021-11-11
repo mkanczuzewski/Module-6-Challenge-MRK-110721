9eb7bf63f663507df12788d2c47d6135b5341256
mkanczuzewski <mkanczuzewski@gmail.com>
2021-11-10 21:56:00 -0500

    fiveday forcast complete

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,57 @@
 var cityFormE1 = document.querySelector("#city-form");
 var cityBtnE1 = document.querySelector("#citybtn");
 var cityNameE1 = document.querySelector("#city");
+var getHistoryBtn = document.querySelector("#historyBtn");
 var cityLat = null;
 var cityLon = null;
-const currentWeatherE2 = document.getElementById('currentWeather')
+const currentWeatherE2 = document.getElementById('currentWeather');
+const fiveDayWeatherE2 = document.getElementById('fiveDayWeather');
+const weatherE2 = document.getElementById('history');
 
 
+//on search click query to first weather api to convert citys to latitude and longitude
 $(document).ready(function() 
 {
+    weatherHistory();
+    //console.log(getHistoryBtn);
+    // getHistoryBtn.forEach(button =>
+    //     {
+    //         button.addEventListener('click', event=>
+    //         {
+    //             console.log(event.target.id)
+    //             // if (event.target.id == correctAns.innerText)
+    //             // {
+    //             //     document.getElementById("correct").style.display = "flex";
+    //             // }
+    //             // else{
+    //             //     document.getElementById("incorrect").style.display = "flex";
+    //             //     timeLeft = timeLeft - 10;
+    
+    //             // }
+    //             // setTimeout(nextQuestion, 1000);
+    //         });
+    //     });
+
     $(cityBtnE1).click(function() 
     {
-        //alert($(cityNameE1).val());
-        //format the open weather API URL
+        //format the open weather "current weather data" API URL
         var cityName = $(cityNameE1).val()
         var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + $.trim(cityName) + '&appid=d318a1a45ba023e17ddc1da41d22b214';
-        console.log(apiUrl)
+        
+        if (localStorage.getItem(cityName) === null)
+        {
+        localStorage.setItem(cityName, JSON.stringify(cityName));
+        }
 
+        weatherHistory();
+        
         fetch(apiUrl)
             .then(function(response)
             {
                 if (response.ok)
                 {
-                    console.log(response);
                     response.json().then(function(data)
                     {
-                        console.log(data);
                         cityLat = data['coord'].lat;
                         cityLon = data['coord'].lon;
                         oneCallApi();
@@ -37,23 +64,35 @@ $(document).ready(function()
     });
 });
 
+function weatherHistory(){
+
+    for (var i = 0; i < localStorage.length; i++)
+    {
+        let weatherStorage = localStorage.key(i);
+        //$(weatherE2).append('<button type="button" class="btn btn-secondary btn-block" id="historyBtn" onclick="bob("'+weatherStorage+'")">'+weatherStorage+'</button>')
+        $(weatherE2).append('<button type="button" class="btn btn-secondary btn-block" id="historyBtn" onclick="getElementById(\'city\').val()="'+weatherStorage+'">'+weatherStorage+'</button>')
+    }
+};
+
+function bob(cityName){
+    console.log(cityName)
+}
+
+//Once city is converted to latitude and longitude, query to second api and build current weather
 function oneCallApi () 
 {
+    //format the open weather "One Call" API URL
     var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=d318a1a45ba023e17ddc1da41d22b214';
-    console.log(apiUrl);
 
     fetch(apiUrl)
         .then(function(response)
         {
             if (response.ok)
             {
-                console.log(response);
                 response.json().then(function(data)
                 {
-                    console.log(data);
                     let curDt = data['current'].dt;
-                    let curDtMil = curDt*1000;
-                    let date = new Date(curDtMil);
+                    let date = new Date(curDt*1000);
                     let curTempE2 = data['current'].temp;
                     let curWindE2 = data['current'].wind_speed;
                     let curHumE2 = data['current'].humidity;
@@ -82,9 +121,41 @@ function oneCallApi ()
                         $(curUV).css("color", "white");
                     }
                 });
+                forecastApi();
             } else 
             {
                 alert('Error: ' + response.statusText);
             }
         })
-}
+};
+
+//Dynamically build five day weather cards
+let forecastApi = function()
+{
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=d318a1a45ba023e17ddc1da41d22b214';
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(function(response)
+        {
+            if (response.ok)
+            console.log(response);
+            {
+                response.json().then(function(data)
+                {
+                    console.log(data);
+                    for (var i = 1; i < 6; i++)
+                    {
+                        let fiveDayDate = data['daily'][i].dt; 
+                        let fiveDate = new Date(fiveDayDate*1000).toLocaleDateString("en-US");
+                        let icons = data['daily'][i].weather[0].icon;
+                        let urlIcons = 'https://openweathermap.org/img/wn/'+icons+'.png';
+                        let fiveDayTemp = data['daily'][i].temp.day;
+                        let fiveDayWind = data['daily'][i].wind_speed;
+                        let fiveDayHumid = data['daily'][i].humidity;
+                        $(fiveDayWeatherE2).append('<div class="col-lg-2 col-md-4"><div class="card"><div class="card-body"><h5 class="card-title">'+fiveDate+'</h5><div><img id="weatherIcon" src='+urlIcons+'></div><div>Temp: '+fiveDayTemp+' °F</div><div>Wind: '+fiveDayWind+' MPH</div><div>Humidity: '+fiveDayHumid+' %</div></div></div></div>')
+                    }
+                });
+            }
+        })    
+};
